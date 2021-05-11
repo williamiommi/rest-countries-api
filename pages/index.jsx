4ff8dcc388bff586b3808ec321d2c84aff06b482
William Iommi <william.iommi@gmail.com>
2021-05-11 10:52:46 +0200

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Rest Countries API</title>
+        <meta name="description" content="Rest Countries API" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
