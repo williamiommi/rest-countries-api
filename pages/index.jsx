f803e97b8a725bc476c14bc309cc04b2d45cdfbc
William Iommi <william.iommi@gmail.com>
2021-05-11 12:04:17 +0200

    tailwind config

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,6 +7,8 @@ export default function Home() {
         <title>Rest Countries API</title>
         <meta name="description" content="Rest Countries API" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,600;0,800;1,300;1,600;1,800&display=swap" rel="stylesheet" />
       </Head>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      gray: {
+        light: 'hsl(0, 0%, 98%)',
+        dark: 'hsl(0, 0%, 52%)',
+      },
+      blue: {
+        text: 'hsl(200, 15%, 8%)',
+        dark: 'hsl(209, 23%, 22%)',
+        veryDark: 'hsl(207, 26%, 17%)'
+      },
+    },
+    screens: {
+      'desktop': '1440px'
+    },
+    fontFamily: {
+      body: ['Nunito Sans', 'sans-serif'],
+    },
+    fontSize: {
+      'base': '14px',
+      'detail': '16px'
+    },
     extend: {},
   },
   variants: {
