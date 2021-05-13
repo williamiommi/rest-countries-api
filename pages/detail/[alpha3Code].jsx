f00495db87c4c0169cf843e2e6c5f102cf649672
William Iommi <william.iommi@gmail.com>
2021-05-13 19:41:21 +0200

    added fallback and 404 on detail page

--- pages/detail/[alpha3Code].jsx
+++ pages/detail/[alpha3Code].jsx
@@ -1,12 +1,27 @@
+import { useRouter } from "next/router";
+
 import getCountriesAlpha3Code from "../../lib/api/getCountriesAlpha3Code";
 import getCountryDetail from "../../lib/api/getCountryDetail";
 
-export default function Detail({ country }) {
+export default function Detail({ country, error }) {
+  const router = useRouter();
+
+  if (router.isFallback)
+    return (
+      <div className="mx-auto text-center p-10 desktop:container">
+        Loading...
+      </div>
+    );
   return <div>{country.nativeName}</div>;
 }
 
 export async function getStaticProps({ params }) {
   const country = await getCountryDetail(params.alpha3Code);
+  if (!country) {
+    return {
+      notFound: true,
+    }
+  }
   return {
     props: {
       country,
@@ -19,6 +34,6 @@ export async function getStaticPaths() {
   const paths = countries.map(({ alpha3Code }) => ({ params: { alpha3Code } }));
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 }
