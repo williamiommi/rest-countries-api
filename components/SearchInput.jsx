d891359505b8ce36c41e1535a5fd9217474580cf
William Iommi <william.iommi@gmail.com>
2021-05-19 14:38:08 +0200

    minor

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -6,6 +6,7 @@ const SearchInput = ({ handleTextSearch }) => {
     <div className="flex flex-row align-middle w-full rounded-md bg-white shadow-md mb-4 dark:bg-blue-dark tablet:mb-0 tablet:w-2/5 desktop:w-2/6">
       <SearchIcon className="w-6 mx-5 text-gray-dark dark:text-white" />
       <input
+        name="input"
         data-testid="input"
         type="text"
         placeholder="Search for a country..."
