f3479d83f9577e5f9f59eaf8188b6b72391f4faf
William Iommi <william.iommi@gmail.com>
2021-05-19 13:47:08 +0200

    changes on theme mode

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,13 @@
-import { MoonIcon } from "@heroicons/react/solid";
+import { useState } from 'react';
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { toggleTheme } from "../lib/utils";
 
 export default function Header() {
+  const [isDark, setIsDark] = useState(false);
+  const Icon = isDark ? SunIcon : MoonIcon;
+  const handleToggle = () => {
+    setIsDark(toggleTheme());
+  }
   return (
     <header className="bg-white shadow-sm dark:bg-blue-dark">
       <div className="mx-auto flex flex-row justify-between px-4 py-6 desktop:container desktop:px-0">
@@ -10,10 +16,11 @@ export default function Header() {
         </p>
         <button
           className="flex flex-row items-center justify-center"
-          onClick={toggleTheme}
+          onClick={handleToggle}
+          title={`switch to ${isDark ? 'light' : 'dark'} mode`}
+          data-testid="switch"
         >
-          <MoonIcon className="w-6 pr-1" />
-          Dark mode
+          <Icon className="w-6 pr-1" />
         </button>
       </div>
     </header>
--- components/Header.test.js
+++ components/Header.test.js
@@ -1,9 +1,19 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Header from "./Header";
 
 describe("Header Component", () => {
   test("it renders", () => {
     const { container } = render(<Header />);
     expect(container).toBeDefined();
+  });
+
+  test("it switch theme mode", () => {
+    const { queryByTestId } = render(<Header />);
+    const btn = queryByTestId('switch');
+    expect(btn).toBeTruthy();
+    expect(btn).toHaveAttribute('title', 'switch to dark mode');
+    fireEvent.click(btn)
+    expect(btn).toHaveAttribute('title', 'switch to light mode');
   });
 });
